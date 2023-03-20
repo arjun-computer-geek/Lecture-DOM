f88b039d061189ba4c931676037bd77e376c288b
arjun <arjun2000raj@gmail.com>
2023-03-20 08:15:16 +0530

    fix : fetch call with async await

--- FetchCall/script.js
+++ FetchCall/script.js
@@ -1,10 +1,11 @@
 const btn = document.querySelector("#btn")
 const container = document.querySelector("#container")
+const fetchBtn = document.querySelector("#asyncBtn")
 
-const url = "https://pokeapi.co/api/v2/pokemon/ditt";
-
+const url = "https://pokeapi.co/api/v2/pokemon/ditto";
+// var loading =false
 btn.addEventListener("click", () => {
-
+    // loading = true;
     // fetch(url)
     //     .then((abc) => abc.json())
     //     .then((data) => {
@@ -17,6 +18,7 @@ btn.addEventListener("click", () => {
     //     })
     //     .finally(() => {
     //         console.log("abcd")
+    // loading = false;
     //     })
 
     fetch(url)
@@ -35,4 +37,20 @@ btn.addEventListener("click", () => {
         .finally(() => {
             console.log("abcd")
         })
+
+})
+
+// async await syntax
+
+fetchBtn.addEventListener("click", async () => {
+    
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log(data)
+
+    } catch (error) {
+        console.log(error);
+    } 
+
 })
