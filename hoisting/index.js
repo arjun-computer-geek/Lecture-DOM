fdaa6bdbf48fd486da93b5daea6e506d496919dc
arjun <arjun2000raj@gmail.com>
2023-03-27 08:10:57 +0530

    Hoisting and Function scope

---
+++ hoisting/index.js
@@ -0,0 +1,36 @@
+// console.log(a);
+// var a = 5;
+
+// hello();
+
+// function hello() {
+//   console.log("hello");
+// }
+
+// var x = 5;
+// a();
+// b();
+// console.log(x);
+
+// function a() {
+//   //   var x = 10;
+//   console.log(x);
+// }
+
+// function b() {
+//   var x = 20;
+//   console.log(x);
+// }
+
+var x = 5;
+a();
+// console.log(x);
+
+function a() {
+  //   var x = 10;
+  function b() {
+    console.log(x);
+  }
+
+  console.log(x);
+}
