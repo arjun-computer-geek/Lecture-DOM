e3a61c9b05c0371b2afd869ce982b3fcfbd8e6c0
arjun <arjun2000raj@gmail.com>
2023-03-31 08:22:18 +0530

    if else and operator

---
+++ ifElseSwitchCase/script.js
@@ -0,0 +1,84 @@
+// if- else syntax
+/*
+if(condition){ //optional
+//body.....
+}
+else{
+body...
+}
+*/
+
+// nested if-else
+// var a = 25;
+// if (a > 5) {
+//     if(a != 0)
+//     if()
+//   console.log("yes");
+// } else {
+//     if(con)
+//   console.log("no");
+// }
+
+//lader-if else
+
+// if(){
+//     console.log("first")
+// }
+// else if(){
+
+// }else if(){
+
+// }else{
+
+// }
+
+// Operators
+
+/*
+=  :  assignment operator
+== : comparision operator
+=== : comparision operator
+!= : not equal to 
+!== : not equal to 
+
+&& : and operator
+|| : or operator
+> : greater than
+>= : greater than or equal to
+< : less than
+<= : less than or equal to
+ */
+
+// examples
+
+var a = 2;
+var b = 0;
+var condition = "";
+// if (a >= 3 && condition && (a = 10)) {
+//   console.log("yes");
+// } else {
+//   console.log("no");
+// }
+// console.log(b, "b");
+// console.log(a, "a");
+
+// if (a >= 3 || condition || (a = 10)) {
+//   console.log("yes");
+// } else {
+//   console.log("no");
+// }
+// console.log(b, "b");
+// console.log(a, "a");
+
+const age = 80;
+const number = 5;
+const arr = [];
+if (-5.5) {
+  if (number > 10) {
+    console.log("greater than 10");
+  } else {
+    console.log("less than 10");
+  }
+} else {
+  console.log("age is not valid");
+}
